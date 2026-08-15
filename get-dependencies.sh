c221a99a0e62c3780d4ed9099e3d14a1a448d69e
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:00:54 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -29,5 +29,3 @@ get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
 #make-aur-package qmmp-plugin-pack
-
-# If the application needs to be manually built that has to be done down here
